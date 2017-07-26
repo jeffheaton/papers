93bfef1aa13a13436c3a38c130b2927a4064382f
Heaton <jheaton@rgare.net>
2017-07-26 17:21:44 -0500

    Updated to use Keras, will soon update paper as well.

--- 2016/ieee-feature-eng/run_experiment.py
+++ 2016/ieee-feature-eng/run_experiment.py
@@ -9,22 +9,20 @@ __author__ = 'jheaton'
 import math
 import numpy as np
 import time
-import types
 import codecs
 import csv
 import multiprocessing
 from sklearn.metrics import mean_squared_error
-from lasagne.layers import DenseLayer
-from lasagne.layers import InputLayer
-from lasagne.nonlinearities import rectify
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
 from sklearn.ensemble.forest import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.callbacks import EarlyStopping
+from keras.wrappers.scikit_learn import KerasRegressor
 
 # Global parameters
 TRAIN_SIZE = 10000
@@ -36,35 +34,6 @@ FAIL_ON_NAN = False
 NORMALIZE_ALL = False
 DUMP_FILES = True
 THREADS = multiprocessing.cpu_count()
-
-
-# Define an early stopping class for the deep learning, once the validation score
-# does not improve for the specified number of epochs, stop and use weights from
-# the best validation score epoch.
-class EarlyStopping(object):
-    def __init__(self, patience=100):
-        self.patience = patience
-        self.best_valid = np.inf
-        self.best_valid_epoch = 0
-        self.best_weights = None
-
-    def __call__(self, nn, train_history):
-        current_valid = train_history[-1]['valid_loss']
-        current_epoch = train_history[-1]['epoch']
-        if math.isnan(current_valid) and FAIL_ON_NAN:
-            raise Exception("Unstable neural network. Can't validate.")
-        if current_valid < self.best_valid:
-            self.best_valid = current_valid
-            self.best_valid_epoch = current_epoch
-            self.best_weights = nn.get_all_params_values()
-        elif self.best_valid_epoch + self.patience < current_epoch:
-            if VERBOSE > 0:
-                print("Early stopping.")
-                print("Best valid loss was {:.6f} at epoch {}.".format(
-                    self.best_valid, self.best_valid_epoch))
-                nn.load_params_from(self.best_weights)
-            raise StopIteration()
-
 
 # Another stopping class for deep learning.  If the error/loss falls
 # below the specified threshold, we are done.
@@ -151,7 +120,7 @@ def generate_data_counts(rows):
 
     for i in range(rows):
         x = [0] * 50
-        y = np.random.randint(0, len(x)+1)
+        y = np.random.randint(0, len(x))
 
         remaining = y
         while remaining > 0:
@@ -164,6 +133,7 @@ def generate_data_counts(rows):
         y_array.append(y)
 
     return np.array(x_array, dtype=np.float32), np.array(y_array, dtype=np.float32)
+
 
 # Generate data for the quadradic experiment, distance between polynomial roots.
 def generate_data_quad(rows):
@@ -187,6 +157,7 @@ def generate_data_quad(rows):
         y_array.append(abs(y[0] - y[1]))
 
     return np.array(x_array, dtype=np.float32), np.array(y_array, dtype=np.float32)
+
 
 # Generate data for a BMI-like feature.
 def generate_data_bmi(rows):
@@ -272,49 +243,15 @@ def generate_data_diff(rows):
 
 # Build a deep neural network for the experiments.
 def neural_network_regression(data):
-    layers0 = [('input', InputLayer),
-               ('dense0', DenseLayer),
-               ('dense1', DenseLayer),
-               ('dense2', DenseLayer),
-               ('dense3', DenseLayer),
-               ('dense4', DenseLayer),
-               ('output', DenseLayer)]
-
-    net0 = NeuralNet(layers=layers0,
-                     input_shape=(None, len(data.X_train[0])),
-                     dense0_num_units=400,
-                     dense0_nonlinearity=rectify,
-                     dense1_num_units=200,
-                     dense1_nonlinearity=rectify,
-                     dense2_num_units=100,
-                     dense2_nonlinearity=rectify,
-                     dense3_num_units=50,
-                     dense3_nonlinearity=rectify,
-                     dense4_num_units=25,
-                     dense4_nonlinearity=rectify,
-                     output_num_units=1,
-                     output_nonlinearity=None,
-
-                     update=nesterov_momentum,
-                     update_learning_rate=0.00001,
-                     update_momentum=0.9,
-                     regression=True,
-
-                     on_epoch_finished=[
-                         EarlyStopping(patience=20),
-                         AcceptLoss(min=0.01)
-                     ],
-
-                     verbose=VERBOSE,
-                     max_epochs=100000)
-
-    # Provide our own validation set
-    def my_split(self, X, y, eval_size):
-        return data.X_train, data.X_validate, data.y_train_nn, data.y_validate_nn
-
-    net0.train_split = types.MethodType(my_split, net0)
-
-    return net0
+    model = Sequential()
+    model.add(Dense(400, input_dim=data.X_train.shape[1], activation='relu'))
+    model.add(Dense(200, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(25, activation='relu'))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
 
 
 # Grid-search for a SVM with good C and Gamma.
@@ -329,8 +266,8 @@ def svr_grid():
 
 
 # Perform an experiment for a single model type.
-def eval_data(writer, name, clf, data):
-    model_name = clf.__class__.__name__
+def eval_data(writer, name, model, data):
+    model_name = model.__class__.__name__
 
     X_validate = data.X_validate
     X_train = data.X_train
@@ -345,12 +282,24 @@ def eval_data(writer, name, clf, data):
         X_train = data.X_train_norm
 
     cycle_list = []
+    v = 1 if VERBOSE > 0 else 0
+
     for cycle_num in range(1, CYCLES + 1):
         start_time = time.time()
-        clf.fit(X_train, y_train)
+        if 'KerasRegressor' in model_name:
+
+            monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=20, verbose=v, mode='auto')
+            model.fit(data.X_train, data.y_train_nn,
+                      validation_data=(data.X_validate, data.y_validate_nn),
+                      callbacks=[monitor], verbose=v, epochs=100000)
+        else:
+            model.fit(X_train, y_train)
         elapsed_time = hms_string(time.time() - start_time)
 
-        pred = clf.predict(X_validate)
+        if 'KerasRegressor' in model_name:
+            pred = model.predict(X_validate,verbose=v)
+        else:
+            pred = model.predict(X_validate)
 
         # Get the validatoin score
         if np.isnan(pred).any():
@@ -358,7 +307,7 @@ def eval_data(writer, name, clf, data):
                 raise Exception("Unstable neural network. Can't validate.")
             score = 1e5
         else:
-            score = mean_squared_error(pred, y_validate)
+            score = np.sqrt(mean_squared_error(pred, y_validate))
 
         line = [name, model_name, score, elapsed_time]
         cycle_list.append(line)
@@ -386,36 +335,38 @@ def run_experiment(writer, name, generate_data):
         svr_grid(),
         RandomForestRegressor(n_estimators=100),
         GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=10, random_state=0, verbose=VERBOSE),
-        neural_network_regression(data)
+        KerasRegressor(build_fn=neural_network_regression, data=data)
     ]
 
     for model in models:
         eval_data(writer, name, model, data)
+
 
 def main():
     with codecs.open("results.csv", "w", "utf-8") as fp:
         writer = csv.writer(fp)
         writer.writerow(['experiment', 'model', 'error', 'elapsed'])
         start_time = time.time()
-        run_experiment(writer, "counts", generate_data_counts)  #1
-        run_experiment(writer, "quad", generate_data_quad)  #2
-        run_experiment(writer, "sqrt", generate_data_fn(1, 1.0, 100.0 ,math.sqrt))  #3
-        run_experiment(writer, "log", generate_data_fn(1, 1.0, 100.0 ,math.log))   #4
-        run_experiment(writer, "pow", generate_data_fn(1, 1.0, 10.0, lambda x: x ** 2))  #5
-        run_experiment(writer, "ratio", generate_data_ratio)  #6
-        run_experiment(writer, "diff", generate_data_diff)  #7
-        run_experiment(writer, "r_poly", generate_data_fn(1, 1.0, 10.0, lambda x: 1/( 5*x + 8 * x ** 2)))  #8
-        run_experiment(writer, "poly", generate_data_fn(1, 0.0, 2.0, lambda x: 1+5*x+8*x**2))   #9
-        run_experiment(writer, "r_diff", generate_data_fn(4, 1.0, 10.0, lambda a,b,c,d: ((a-b)/(c-d))))  #10
+        run_experiment(writer, "counts", generate_data_counts)  # 1
+        run_experiment(writer, "quad", generate_data_quad)  # 2
+        run_experiment(writer, "sqrt", generate_data_fn(1, 1.0, 100.0, math.sqrt))  # 3
+        run_experiment(writer, "log", generate_data_fn(1, 1.0, 100.0, math.log))  # 4
+        run_experiment(writer, "pow", generate_data_fn(1, 1.0, 10.0, lambda x: x ** 2))  # 5
+        run_experiment(writer, "ratio", generate_data_ratio)  # 6
+        run_experiment(writer, "diff", generate_data_diff)  # 7
+        run_experiment(writer, "r_poly", generate_data_fn(1, 1.0, 10.0, lambda x: 1 / (5 * x + 8 * x ** 2)))  # 8
+        run_experiment(writer, "poly", generate_data_fn(1, 0.0, 2.0, lambda x: 1 + 5 * x + 8 * x ** 2))  # 9
+        run_experiment(writer, "r_diff", generate_data_fn(4, 1.0, 10.0, lambda a, b, c, d: ((a - b) / (c - d))))  # 10
 
         # Others to try, not in the paper.
-        #run_experiment(writer, "sum", generate_data_fn(10, 0.0, 10.0, lambda *args: np.sum(args)))
-        #run_experiment(writer, "max", generate_data_fn(10, 0.0, 100.0, lambda *args: np.max(args)))
-        #run_experiment(writer, "dev", generate_data_fn(10, 0.0, 100.0, lambda *args: np.std(args)))
-        #run_experiment(writer, "bmi", generate_data_bmi)  #### 2
+        # run_experiment(writer, "sum", generate_data_fn(10, 0.0, 10.0, lambda *args: np.sum(args)))
+        # run_experiment(writer, "max", generate_data_fn(10, 0.0, 100.0, lambda *args: np.max(args)))
+        # run_experiment(writer, "dev", generate_data_fn(10, 0.0, 100.0, lambda *args: np.std(args)))
+        # run_experiment(writer, "bmi", generate_data_bmi)  #### 2
 
         elapsed_time = time.time() - start_time
         print("Elapsed time: {}".format(hms_string(elapsed_time)))
+
 
 # Allow windows to multi-thread (unneeded on advanced OS's)
 # See: https://docs.python.org/2/library/multiprocessing.html
